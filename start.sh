c2a54d36793db46f9a9894c62fda872004484a1a
cvatricks <68840422+cvatricks@users.noreply.github.com>
2022-04-06 08:16:18 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone -b v1.0 https://github.com/cvatricks/EvaMaria.git /EvaMaria
+  git clone -b v1.0 https://github.com/cvatricks/EvaMariaa.git /EvaMariaa
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /EvaMariaa
 fi
-cd /EvaMaria
+cd /EvaMariaa
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
