025048e877efcf48a7d3733636c7d7e0bc4b217d
cvatricks <68840422+cvatricks@users.noreply.github.com>
2022-04-06 08:22:02 -0700

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -25,6 +25,7 @@ async def start(client, message):
             [
                 InlineKeyboardButton('📍 Channel', url='https://t.me/M2LMOVIEZ')
             ]
+        ]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply(script.START_TXT.format(message.from_user.mention if message.from_user else message.chat.title, temp.U_NAME, temp.B_NAME), reply_markup=reply_markup)
         await asyncio.sleep(2) # 😢 https://github.com/EvamariaTG/EvaMaria/blob/master/plugins/p_ttishow.py#L17 😬 wait a bit, before checking.
