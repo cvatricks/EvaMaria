5e2541bc8e033d44a140022f6d7a6aed35104c5b
cvatricks <68840422+cvatricks@users.noreply.github.com>
2022-05-01 14:57:39 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -8,7 +8,7 @@ from pyrogram.errors import ChatAdminRequired, FloodWait
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from database.ia_filterdb import Media, get_file_details, unpack_new_file_id
 from database.users_chats_db import db
-from info import CHANNELS, ADMINS, AUTH_CHANNEL, LOG_CHANNEL, PICS, BATCH_FILE_CAPTION, CUSTOM_FILE_CAPTION, PROTECT_CONTENT, GROUP_USERNAME
+from info import CHANNELS, ADMINS, AUTH_CHANNEL, LOG_CHANNEL, PICS, BATCH_FILE_CAPTION, CUSTOM_FILE_CAPTION, PROTECT_CONTENT, GROUP_LINK
 from utils import get_settings, get_size, is_subscribed, save_group_settings, temp
 from database.connections_mdb import active_connection
 import re
@@ -21,7 +21,6 @@ BATCH_FILES = {}
 @Client.on_message(filters.command("start") & filters.incoming & ~filters.edited)
 async def start(client, message):
     if message.chat.type in ['group', 'supergroup']:
-        GROUP_LINK = "https://t.me/{}".format(str(GROUP_USERNAME))
         buttons = [
             [
                 InlineKeyboardButton('CHANNEL', url='https://t.me/M2LMOVIEZ'),
@@ -40,7 +39,6 @@ async def start(client, message):
         await db.add_user(message.from_user.id, message.from_user.first_name)
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
-        GROUP_LINK = "https://t.me/{}".format(str(GROUP_USERNAME))
         buttons = [
             [
                 InlineKeyboardButton('CHANNEL', url='https://t.me/M2LMOVIEZ'),
@@ -80,7 +78,6 @@ async def start(client, message):
             )
         return
     if len(message.command) == 2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
-        GROUP_LINK = "https://t.me/{}".format(str(GROUP_USERNAME))
         buttons = [
             [
                 InlineKeyboardButton('CHANNEL', url='https://t.me/M2LMOVIEZ'),
