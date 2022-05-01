44f2a5a00055b824303c49bb414d3a2e9b159d46
cvatricks <68840422+cvatricks@users.noreply.github.com>
2022-05-01 14:13:26 +0530

    Update index.py

--- plugins/index.py
+++ plugins/index.py
@@ -146,10 +146,10 @@ async def index_files_to_db(lst_msg_id, chat, msg, bot):
             current = temp.CURRENT
             temp.CANCEL = False
             try:
-               list_msg = await bot.iter_messages(chat, lst_msg_id, temp.CURRENT)
+               list_msg = async bot.iter_messages(chat, lst_msg_id, temp.CURRENT)
             except FloodWait as f:
                await asyncio.sleep(f.x)
-               list_msg = await bot.iter_messages(chat, lst_msg_id, temp.CURRENT)
+               list_msg = async bot.iter_messages(chat, lst_msg_id, temp.CURRENT)
             for message in list_msg:
                 if temp.CANCEL:
                     await msg.edit(f"Successfully Cancelled!!\n\nSaved <code>{total_files}</code> files to dataBase!\nDuplicate Files Skipped: <code>{duplicate}</code>\nDeleted Messages Skipped: <code>{deleted}</code>\nNon-Media messages skipped: <code>{no_media + unsupported}</code>(Unsupported Media - `{unsupported}` )\nErrors Occurred: <code>{errors}</code>")
