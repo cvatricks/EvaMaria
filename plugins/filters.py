68c2286313aa2048681738ead266af6994f35a46
CvaTricks <68840422+cvatricks@users.noreply.github.com>
2022-09-08 22:04:11 +0530

    Update filters.py

--- plugins/filters.py
+++ plugins/filters.py
@@ -35,7 +35,7 @@ async def addfilter(client, message):
             await message.reply_text("I'm not connected to any groups!", quote=True)
             return
 
-    elif chat_type in [[enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]]:
+    elif chat_type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
         grp_id = message.chat.id
         title = message.chat.title
 
